a2975f24b6a494083dfdfd7ea1cc694ec85a7e57
Budi Gunawan <budiindra.g@gmail.com>
2024-07-12 11:03:01 +0700

    feat: add product schema

--- src/schemas/product-schema.ts
+++ src/schemas/product-schema.ts
@@ -24,3 +24,65 @@ export const ProductIdSchema = z.object({
     .min(5)
     .openapi({ example: "clxsclxli0000vs7d0wqe3y8k" }),
 });
+
+export const CreateProductSchema = z.object({
+  name: z.string().min(5).openapi({ example: "T-shirt" }),
+  price: z.number().min(1000).openapi({ example: 10000 }),
+  stock: z.number().min(0).openapi({ example: 10 }),
+  imageUrl: z
+    .string()
+    .url()
+    .openapi({ example: "https://gudegyudjum.com" })
+    .optional()
+    .or(z.literal("")),
+  size: z
+    .string()
+    .openapi({ example: "New born" })
+    .optional()
+    .or(z.literal("")),
+  slug: z.string().openapi({ example: "t-shirt" }).optional().or(z.literal("")),
+  sku: z.string().openapi({ example: "TSNB001" }).optional().or(z.literal("")),
+  overview: z
+    .string()
+    .max(500)
+    .openapi({ example: "T-shirt for newborn baby" })
+    .optional()
+    .or(z.literal("")),
+  materials: z
+    .string()
+    .max(500)
+    .openapi({ example: "Cotton" })
+    .optional()
+    .or(z.literal("")),
+});
+
+export const UpdateProductSchema = z.object({
+  name: z.string().min(5).openapi({ example: "T-shirt" }),
+  price: z.number().min(1000).openapi({ example: 10000 }),
+  stock: z.number().min(0).openapi({ example: 10 }),
+  imageUrl: z
+    .string()
+    .url()
+    .openapi({ example: "https://gudegyudjum.com" })
+    .optional()
+    .or(z.literal("")),
+  size: z
+    .string()
+    .openapi({ example: "New born" })
+    .optional()
+    .or(z.literal("")),
+  slug: z.string().openapi({ example: "t-shirt" }).optional().or(z.literal("")),
+  sku: z.string().openapi({ example: "TSNB001" }).optional().or(z.literal("")),
+  overview: z
+    .string()
+    .max(500)
+    .openapi({ example: "T-shirt for newborn baby" })
+    .optional()
+    .or(z.literal("")),
+  materials: z
+    .string()
+    .max(500)
+    .openapi({ example: "Cotton" })
+    .optional()
+    .or(z.literal("")),
+});
